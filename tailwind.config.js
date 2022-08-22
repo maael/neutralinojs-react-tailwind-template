d8338fedb387d040f48e8e5e95dec1ba0ac97791
Matthew Elphick <matt.a.elphy@gmail.com>
2022-08-22 23:42:17 +0100

    chore: Bump packages, update config update script (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  purge: [
-    "./src/**/*.tsx",
-  ],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
